cc77c44651e4e0651ca2378598c6137b8670f173
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-12-21 16:32:46 +0000

    solved problem1day2

---
+++ 2a. fibonacci.py
@@ -0,0 +1,8 @@
+def fiboRecurse(n, sequence=[0, 1]):
+    if n <= len(sequence):
+        return sequence[:n]
+    sequence.append(sequence[-1] + sequence[-2])
+    return fiboRecurse(n, sequence)
+n = int(input("Enter the value of n: "))
+sequence = fiboRecurse(n)
+print(f"Fibonacci sequence up to the {n}th term: {sequence}")
