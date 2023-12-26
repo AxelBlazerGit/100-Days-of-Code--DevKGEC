ff46972c58a4e3eb7c4854c94194ec15b67b7ff8
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-12-26 10:31:03 +0000

    day4 solved

--- day4/4d. num2word.py
+++ day4/4d. num2word.py
@@ -1,21 +1,23 @@
-one=["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine","ten"]
+one=["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 two=["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 misc=["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 def words(n):
     word=''
-    if n>19:
-        word+=one[n%10]
-        n//=10
-        word=two[n%10]+' '+word
-        n//=10
+    if n>=1000:
+        word+=one[n//1000] + " thousand "
+        n %= 1000
+    if n>=100:
+        word+=one[n//100] + " hundred "
+        n %= 100
+    if n>0:
+        if n>=20:
+            word+=two[n//10]
+            n %= 10
+        elif n>=11:
+            word+=misc[n-10]
+            n=0
         if n>0:
-            word=one[n%10]+' hundred '+word
-        n//=10
-        if n>0:
-            word=one[n%10]+" thousand "+word
-    elif n<11:
-        return one[n]
-    else: 
-        return misc[n-10]
-    return word;
-print(words(int(input("Enter a no(up to 4 digits): "))))
+            word+=' '+one[n]
+    return word.strip()
+number=int(input("Enter a number(up to 4 digits): "))
+print(words(number))
