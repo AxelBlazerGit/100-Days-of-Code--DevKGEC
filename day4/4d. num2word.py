f1f57174a4146bcb0c7b07588d6d9816251a7273
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-12-26 10:39:48 +0000

    day4 solved

--- day4/4d. num2word.py
+++ day4/4d. num2word.py
@@ -2,22 +2,23 @@ one=["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 two=["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 misc=["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 def words(n):
+    if n==0:
+        return 'zero'
     word=''
     if n>=1000:
-        word+=one[n//1000] + " thousand "
+        word+=one[n//1000] + " thousand"
         n %= 1000
     if n>=100:
-        word+=one[n//100] + " hundred "
+        word+=' '+one[n//100] + " hundred"
         n %= 100
     if n>0:
         if n>=20:
-            word+=two[n//10]
+            word+=' '+two[n//10]
             n %= 10
         elif n>=11:
-            word+=misc[n-10]
+            word+=' '+misc[n-10]
             n=0
         if n>0:
             word+=' '+one[n]
     return word.strip()
-number=int(input("Enter a number(up to 4 digits): "))
-print(words(number))
+print(words(int(input("Enter a number(up to 4 digits): "))))
